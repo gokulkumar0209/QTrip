d5417bdba099a966671d20b748fdc0a1da573b9d
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-07-30 17:20:04 +0530

    Added reviews

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Review({ review }) {
+	const [positive, setPositive] = useState([]);
+	useEffect(() => {
+		const arr = [];
+		for (let i = 0; i < review.rating; i++) {
+			arr.push("⭐");
+		}
+		setPositive(arr);
+	}, []);
 	return (
 		<div className=" bg-white shadow-lg rounded-lg p-4 flex flex-col text-center hover:scale-105">
 			<img src={review.img}></img>
 			<h1 className=" uppercase my-2 font-semibold text-xl">{review.name}</h1>
+			<h2 className=" flex justify-center">
+				{positive.map((star, index) => (
+					<h3 key={index}>{star}</h3>
+				))}
+			</h2>
 			<h2>{review.rev}</h2>
+			
 		</div>
 	);
 }
