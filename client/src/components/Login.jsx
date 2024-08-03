bd4abd1723f2a51e9fac256cbc739871190dd4db
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-03 22:33:23 +0530

    Login and Signup completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 function Login() {
+	const loginUrl = import.meta.env.VITE_POST_LOGIN;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState("");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(loginUrl, {
+				email: email,
+				password: password,
+			});
+			if (res.status == 201) {
+				setMessage("Login Success");
+			}
+		} catch (error) {
+			setMessage(error.res);
+		}
+	};
 	return (
 		<div>
-			<div className="grid grid-cols-1">
-				<label for="username">Email</label>
-				<input type="email" id="username" name="username"></input>
-				<label for="password">Password</label>
-				<input type="password" id="password" name="password"></input>
-				<button type="submit">Login</button>
-			</div>
+			<form onSubmit={handleSubmit}>
+				<div className="grid grid-cols-1">
+					<label for="username">Email</label>
+					<input
+						type="email"
+						id="username"
+						name="username"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></input>
+					<label for="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></input>
+					<button type="submit">Login</button>
+				</div>
+			</form>
+			{message && <p>{message}</p>}
 		</div>
 	);
 }
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,17 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 function Signup() {
+	const registerUrl = import.meta.env.VITE_POST_REGISTER;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmpassword, setconfirmpassword] = useState("");
+	const [message, setMessage] = useState("");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(registerUrl, {
+				email,
+				password,
+				confirmpassword: confirmpassword,
+			});
+			if (res.status == 201) {
+				setMessage("Success");
+			}
+		} catch (error) {
+			setMessage(error.res);
+		}
+	};
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className="grid grid-cols-1">
 					<label for="username">Email</label>
-					<input type="email" id="username" name="username"></input>
+					<input
+						type="email"
+						id="username"
+						name="username"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></input>
 					<label for="password">Password</label>
-					<input type="password" id="password" name="password"></input>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></input>
+					<label for="confirmpassword">confirmpassword</label>
+					<input
+						type="confirmpassword"
+						id="confirmpassword"
+						name="confirmpassword"
+						value={confirmpassword}
+						onChange={(e) => setconfirmpassword(e.target.value)}
+					></input>
 					<button type="submit">Signup</button>
 				</div>
 			</form>
+			{message && <p>{message}</p>}
 		</div>
 	);
 }
