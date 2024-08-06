c9362d13d17f57db109791f2238b4cbd871d0d77
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-06 12:35:10 +0530

    Cities done

--- client/src/components/Cities.jsx
+++ client/src/components/Cities.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
-import dotenv from "dotenv";
 import City from "./City";
+import axios from "axios";
 
 function Cities() {
 	const getCitiesUrl = import.meta.env.VITE_GET_CITIES;
 	const [cities, setCities] = useState([]);
-	// console.log(getCitiesUrl);
 
-	function fetchData() {
-		return fetch(getCitiesUrl)
-			.then((res) => res.json())
-			.then((data) => {
-				setCities(data); // Assign the fetched data to arr // Log the fetched data inside the fetch callback
-				return data; // Return data to handle it further if needed
-			});
+	async function fetchData() {
+		try {
+			const res = await axios.get(getCitiesUrl);
+			setCities(res.data);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 	useEffect(() => {
 		fetchData();
@@ -33,17 +32,12 @@ function Cities() {
 					<span className=" bg-black px-2 rounded-md">S</span>
 				</h1>
 			</div>
-			<div className="grid grid-cols-4 gap-4 mt-4 ml-4 mr-4">
-				{cities.map(
-					(
-						city,
-						index // Added index parameter for unique key
-					) => (
-						<div key={index} className="">
-							<City city={city} />
-						</div>
-					)
-				)}
+			<div className="grid grid-cols-4">
+				{cities.map((city, index) => (
+					<div key={index} className="">
+						<City city={city} />
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -1,73 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function City({ city }) {
-	function setCity(e) {
+	const navigate = useNavigate();
+	const handleSelect = () => {
 		localStorage.setItem("city", city.id);
-	}
+		navigate("/adventures");
+	};
+
 	return (
-		<div className="shadow-lg p-4 rounded-lg bg-white">
-			<div className="flex justify-center mb-4">
+		<div
+			className="shadow-lg p-4 m-4 rounded-lg bg-white transition-transform  ease-in-out hover:scale-105 hover:bg-slate-200 cursor-pointer"
+			onClick={handleSelect}
+		>
+			<div className="flex justify-center m-2">
 				<img
 					className="h-40 w-40 object-cover rounded-full"
 					src={city.image}
 					alt={`Image of ${city.city}`}
 				/>
 			</div>
-			<div className="text-center mb-4">
+			<div className="text-center m-2">
 				<h1 className="text-2xl font-bold uppercase">{city.city}</h1>
 			</div>
-			<div className="text-center mb-4">
+			<div className="text-center m-2">
 				<p className="text-gray-700">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 				</p>
 			</div>
-			<div className="text-center mb-4">
+			<div className="text-center m-2">
 				<p className="text-gray-600">{city.description}</p>
 			</div>
-			<div className="flex justify-center">
-				<Link to="/adventures">
-					<button
-						className="bg-black text-white px-4 py-2 rounded"
-						onClick={(e) => {
-							setCity(e);
-						}}
-					>
-						Explore Now
-					</button>
-				</Link>
-			</div>
+			
 		</div>
-
-		// <div className=" shadow-lg p-2 rounded-lg ">
-		// 	<div className=" flex justify-center">
-		// 		<img
-		// 			className=" h-40 w-40 object-cover flex justify-center"
-		// 			src={city.image}
-		// 		></img>
-		// 	</div>
-		// 	<div className=" uppercase ">
-		// 		<h1 className=" uppercase flex justify-center">{city.city}</h1>
-		// 	</div>
-		// 	<div className=" flex justify-center items-center">
-		// 		<p className=" flex justify-center">
-		// 			Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
-		// 		</p>
-		// 	</div>
-		// 	<div className=" flex justify-center">{city.description}</div>
-		// 	<div className=" flex justify-center">
-		// 		<Link to="/adventures">
-		// 			<button
-		// 				className=" bg-black text-white flex justify-center"
-		// 				onClick={(e) => {
-		// 					setCity(e);
-		// 				}}
-		// 			>
-		// 				Explore Now
-		// 			</button>
-		// 		</Link>
-		// 	</div>
-		// </div>
 	);
 }
 
